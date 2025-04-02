c524d4cef05a7701dc7df72de8afbbfc5870219b
Andrea Rizzi <andrea.rizzi@cnhind.com>
2025-04-02 22:59:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function addTask() {
+    const input = document.getElementById('taskInput');
+    const task = input.value.trim();
+    
+    if (task) {
+        const list = document.getElementById('taskList');
+        const li = document.createElement('li');
+        li.textContent = task;
+        list.appendChild(li);
+        input.value = '';
+    }
+}
